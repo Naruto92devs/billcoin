055728c68c95c6de523ce1a0889381af87a3ada5
Naruto92devs <m.suhaibmpdgw@gmail.com>
2024-06-05 01:32:50 +0500

    converting into react

---
+++ src/App.js
@@ -0,0 +1,231 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <section className="hero-section">
+        <header>
+          <div className="logo">
+            <img src="" alt="" />
+          </div>
+          <div className="socials">
+            <a href="#" className="btn">
+              Copy Coin Address
+            </a>
+            <a href="https://t.me/thebillcoineth">
+              <img src="/images/telegram.png" alt="" />
+            </a>
+            <a href="https://twitter.com/thebillcoineth">
+              <img src="/images/x-twitter.png" alt="" />
+            </a>
+            <a href="">
+              <img src="/images/uniswap.png" alt="" />
+            </a>
+            <a href="">
+              <img src="/images/dextools.png" alt="" />
+            </a>
+          </div>
+        </header>
+        <h1 style={{ color: "#fff" }}>BILL COIN</h1>
+        <h1 className="h-1">
+          BILL <span>COIN</span>
+        </h1>
+        <p>
+          Hello my name is Bill, some call me Bill
+          <br />
+          Привет, меня зовут Джо, некоторые зовут меня Джо.
+          <br />
+          向下滚动或
+          <br />
+          scroll down or 100 years of bad luck
+          <br />
+          向下滚动或 100 年霉运
+        </p>
+        <a href="#">
+          <img src="assets/images/buy-btn.png" alt="" />
+        </a>
+        <img src="/images/cloud-1.png" alt="" className="cloud-1" />
+        <img src="/images/pink-boxes.png" alt="" className="pink-box" />
+        <img src="/images/character-1.png" alt="" className="charac-1" />
+        <img src="/images/character-2.png" alt="" className="charac-2" />
+        <img src="/images/greenish-box.png" alt="" className="greenish-box" />
+        <img src="/images/character-3.png" alt="" className="charac-3" />
+        <img src="/images/character-4.png" alt="" className="charac-4" />
+        <div className="curve-div">
+          <marquee behavior="scroll" direction="left">
+            <p>
+              help help help help help help help help help help help helphelp help
+              help help help help help help help
+            </p>
+          </marquee>
+        </div>
+      </section>
+      <section className="buy">
+        <img src="/images/character-6.png" alt="" className="charac-6" />
+        <img src="/images/cloud-2.png" alt="" className="cloud-2" />
+        <img src="/images/sticker-1.png" alt="" className="sticker-1" />
+        <div className="sticker-2">
+          <span>帳單帳單帳單帳單帳單帳單帳</span>
+          <span>帳單帳單帳單帳單帳單帳單帳單帳單帳單帳單</span>
+          <span>帳單帳單帳</span>
+        </div>
+        <div className="buy-box">
+          <div className="left">
+            <div>
+              <h2>Buy</h2>
+              <h6>
+                <span>$BILL ON</span>
+                <span>DEXTOOLS</span>
+              </h6>
+            </div>
+            <a href="" className="btn-2">
+              Dextools
+            </a>
+          </div>
+          <div className="right">
+            <div>
+              <h2>Buy</h2>
+              <h6>
+                <span>$BILL ON</span>
+                <span>Uniswap</span>
+              </h6>
+            </div>
+            <a href="" className="btn-2">
+              Uniswap
+            </a>
+          </div>
+        </div>
+        <div className="billpaper">
+          <div className="left">
+            <h5>
+              宇宙總統比爾登月路線圖
+              <br />
+              $BILL
+            </h5>
+          </div>
+          <div className="right">
+            <img src="/images/character-5.png" alt="" />
+          </div>
+        </div>
+      </section>
+      <section className="img-group">
+        <img src="/images/character-7.png" alt="" className="charac-7" />
+        <img src="/images/string-1.png" alt="" className="string-1" />
+        <div className="parent-img">
+          <div className="child-img">
+            <img src="/images/img-1.png" alt="" />
+            <img src="/images/img-2.png" alt="" />
+            <img src="/images/img-3.png" alt="" />
+          </div>
+        </div>
+      </section>
+      <section className="parent-container">
+        <img src="/images/pipe.png" alt="" className="pipe-1" />
+        <img src="/images/cloud-1.png" alt="" className="cloud-3" />
+        <img src="/images/character-8.png" alt="" className="charac-8" />
+        <img src="/images/string-2.png" alt="" className="string-2" />
+        <img src="/images/string-3.png" alt="" className="string-3" />
+        <div className="curve-div-2">
+          <marquee behavior="scroll" direction="right">
+            <p>
+              help help help helphelp help help help help help help helphelp help
+              help help help help help help help help help help
+            </p>
+          </marquee>
+        </div>
+        <div className="container container-1">
+          <div className="left">
+            <h6>世界性的革命</h6>
+            <p>
+              What’s the difference between a bill and a $BILL? One is an
+              all-knowing ever present god-like being, and the other is a piece of
+              paper.&nbsp;Vote&nbsp;for&nbsp;BILL!
+            </p>
+          </div>
+          <div className="right">
+            <img src="/images/img-4.png" alt="" />
+          </div>
+        </div>
+        <div className="container container-2">
+          <div className="right">
+            <img src="/images/img-5.png" alt="" />
+          </div>
+          <div className="left">
+            <h6>馬裡奧·洛斯比爾六萬美元</h6>
+            <p>
+              This is Bill’s friend $MARIO. Mario loves to gamble, and owes bill
+              $60,000. If he dont pay back soon, bill gonna&nbsp;kill&nbsp;mario
+            </p>
+          </div>
+        </div>
+      </section>
+      <section className="dance">
+        <img src="/images/bill-coin.png" alt="" className="dance-head" />
+        <img src="/images/pipe-2.png" alt="" className="pipe-2" />
+        <img src="/images/cloud-1.png" alt="" className="cloud-4" />
+        <img src="/images/character-9.png" alt="" className="charac-9" />
+        <img src="/images/folding-page.png" alt="" className="folding-page" />
+        <div className="sticker-3">
+          <span>月亮月亮月亮月</span>
+          <span>月亮月亮月亮月亮月亮月亮月亮月亮月亮月亮</span>
+          <span>月亮月亮月亮月亮月亮月月亮</span>
+        </div>
+        <div className="box">
+          <div className="left">
+            <p>
+              $BILL is family friendly and for all ages. <br />
+              Bill practices freedom of religion
+              <br /> and&nbsp;eats&nbsp;broccoli
+            </p>
+            <img src="/images/img-6.png" alt="" />
+          </div>
+          <div className="right">
+            <p>
+              Once $BILL reaches 1M marketcap BILL will
+              <br /> dance naked on tiktok&nbsp;live&nbsp;stream
+            </p>
+            <img src="/images/img-7.png" alt="" />
+          </div>
+        </div>
+        <div className="box-2">
+          <img src="/images/img-8.png" alt="" />
+        </div>
+      </section>
+      <footer>
+        <img src="/images/character-2.png" alt="" className="charac-10" />
+        <img src="/images/character-1.png" alt="" className="charac-11" />
+        <img src="/images/character-4.png" alt="" className="charac-12" />
+        <p>
+          Hello my name is Bill, some call me Bill
+          <br />
+          Привет, меня зовут Джо, некоторые зовут меня Джо.
+          <br />
+          向下滚动或
+          <br />
+          scroll down or 100 years of bad luck
+          <br />
+          向下滚动或 100 年霉运
+        </p>
+        <div className="socials">
+          <a href="#" className="btn" value="x0000000">
+            Copy Coin Address
+          </a>
+          <a href="https://t.me/thebillcoineth">
+            <img src="assets/images/telegram.png" alt="" />
+          </a>
+          <a href="https://twitter.com/thebillcoineth">
+            <img src="/images/x-twitter.png" alt="" />
+          </a>
+          <a href="#">
+            <img src="/images/uniswap.png" alt="" />
+          </a>
+          <a href="#">
+            <img src="/images/dextools.png" alt="" />
+          </a>
+        </div>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
